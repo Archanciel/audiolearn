5534047cce91256ebf67d07f63b3fd4953fb87c9
Archanciel <jp.schnyder@gmail.com>
2024-05-12 22:02:17 +0200

    ok

--- test/util/dir_util_test.dart
+++ test/util/dir_util_test.dart
@@ -5,11 +5,10 @@ import 'package:audiolearn/utils/dir_util.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path/path.dart' as path;
 
-
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('DirUtil replacePlaylistRootPathInSettingsJsonFiles test)', () {
+  group('DirUtil test)', () {
     test(
       'replacing "/storage/emulated/0/Download/audiolear" by "C:\\Users\\Jean-Pierre\\Development\\Flutter\\audiolearn\\test\\data\\audio"',
       () {
@@ -46,6 +45,33 @@ void main() {
           rootPath: kPlaylistDownloadRootPathWindowsTest,
           deleteSubDirectoriesAsWell: true,
         );
+      },
+    );
+    test(
+      'deleteFilesAndSubDirsOfDir',
+      () {
+        // Purge the test playlist directory if it exists so that the
+        // playlist list is empty
+        DirUtil.deleteFilesInDirAndSubDirs(
+          rootPath: kPlaylistDownloadRootPathWindowsTest,
+          deleteSubDirectoriesAsWell: true,
+        );
+
+        // Copy the test initial audio data to the app dir
+        DirUtil.copyFilesFromDirAndSubDirsToDirectory(
+          sourceRootPath:
+              "$kDownloadAppTestSavedDataDir${path.separator}dir_util_test",
+          destinationRootPath: kPlaylistDownloadRootPathWindowsTest,
+        );
+
+        DirUtil.deleteFilesAndSubDirsOfDir(
+          rootPath: kPlaylistDownloadRootPathWindowsTest,
+        );
+
+        Directory dir = Directory(kPlaylistDownloadRootPathWindowsTest);
+
+        expect(dir.existsSync(), true);
+        expect(dir.listSync().isEmpty, true);
       },
     );
   });
