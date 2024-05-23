f1cedbd03f80a3192f482bc71f20fd4b1092b0f7
Archanciel <jp.schnyder@gmail.com>
2024-05-24 00:38:52 +0200

    corrected by ChatGPT and applied using  main_arb_file_correction.dart located in dart_explore project

--- lib/constants.dart
+++ lib/constants.dart
@@ -15,7 +15,7 @@ enum AudioLearnAppViewType {
 }
 
 const String kApplicationName = "Audio Learn";
-const String kApplicationVersion = '1.1.43';
+const String kApplicationVersion = '1.1.44';
 const String kApplicationPath = "/storage/emulated/0/Download/audiolearn";
 const String kApplicationPathTest = "/storage/emulated/0/Download/audiolearn";
 const String kPlaylistDownloadRootPath =
