f9b1fa3b59fef48e809ca9248727671dbde9905f
Archanciel <jp.schnyder@gmail.com>
2024-08-10 15:25:45 +0200

    All tests pass.

--- lib/constants.dart
+++ lib/constants.dart
@@ -28,7 +28,7 @@ enum AudioPopupMenuAction {
 }
 
 const String kApplicationName = "Audio Learn";
-const String kApplicationVersion = '1.1.81';
+const String kApplicationVersion = '1.1.82';
 const String kApplicationPath = "/storage/emulated/0/Download/audiolearn";
 const String kApplicationPathTest = "/storage/emulated/0/Download/audiolearn";
 const String kPlaylistDownloadRootPath =
