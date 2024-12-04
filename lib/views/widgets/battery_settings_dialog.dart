1a302a8c6932731ff3ad4523eb8de08ac6e22e4a
Archanciel <jp.schnyder@gmail.com>
2024-12-04 21:47:35 +0100

    improved battery setting dialog

--- lib/views/widgets/battery_settings_dialog.dart
+++ lib/views/widgets/battery_settings_dialog.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:app_settings/app_settings.dart';
 
+import '../../constants.dart';
+
 class BatterySettingsDialog extends StatelessWidget {
   const BatterySettingsDialog({super.key});
 
@@ -13,15 +15,32 @@ class BatterySettingsDialog extends StatelessWidget {
           AppLocalizations.of(context)!.batteryParameters,
         ),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Open the app's settings page
-            AppSettings.openAppSettings();
-          },
-          child: Text(
-            AppLocalizations.of(context)!.disableBatteryOptimisation,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 40),
+            Text(
+              AppLocalizations.of(context)!.disableBatteryOptimisation,
+              style: kDialogTextFieldStyle,
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // Open the app's settings page
+                AppSettings.openAppSettings();
+              },
+              child: Text(
+                AppLocalizations.of(context)!.openBatteryOptimisationButton,
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kButtonColor, // Set button color in dark mode
+                foregroundColor:
+                    Colors.white, // Set button text color in dark mode
+              ),
+            ),
+          ],
         ),
       ),
     );
