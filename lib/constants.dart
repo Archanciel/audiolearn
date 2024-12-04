c006073a4e623caaac56181e5157d6defe34eb79
Archanciel <jp.schnyder@gmail.com>
2024-12-04 11:26:25 +0100

    suppression de wakelock_plus. apk generation works and play next after modifying Android parms app param works.

--- lib/constants.dart
+++ lib/constants.dart
@@ -28,7 +28,7 @@ enum AudioPopupMenuAction {
 }
 
 const String kApplicationName = "Audio Learn";
-const String kApplicationVersion = '1.2.851';
+const String kApplicationVersion = '1.2.86';
 const String kApplicationPath = "/storage/emulated/0/Download/audiolearn";
 const String kApplicationPathTest = "/storage/emulated/0/Download/audiolearn";
 const String kPlaylistDownloadRootPath =
