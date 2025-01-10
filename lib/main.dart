76384170fc957c312f6e29b965d9975103598cd4
Archanciel <jp.schnyder@gmail.com>
2025-01-10 22:32:51 +0100

    all tests in audio_player_view_integration_test.dart now pass

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ Future<void> setWindowsAppSizeAndPosition({
 
       // Définissez la largeur et la hauteur de votre fenêtre
       double windowWidth = (isTest) ? 900 : 730;
-      const double windowHeight = 1300;
+      double windowHeight = (isTest) ? 1400 : 1300;
 
       // Calculez la position X pour placer la fenêtre sur le côté droit de l'écran
       final double posX = screenRect.right - windowWidth + 10;
