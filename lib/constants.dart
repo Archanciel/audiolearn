656ffa0300ad5c7c72a4eb2aea827e0f26e87ad8
Archanciel <jp.schnyder@gmail.com>
2025-01-16 17:32:16 +0100

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -30,7 +30,7 @@ enum AudioPopupMenuAction {
 }
 
 const String kApplicationName = "Audio Learn";
-const String kApplicationVersion = '1.3.18';
+const String kApplicationVersion = '1.3.19';
 const String kApplicationPath = "/storage/emulated/0/Download/audiolearn";
 const String kApplicationPathTest = "/storage/emulated/0/Download/audiolearn";
 const String kPlaylistDownloadRootPath =
