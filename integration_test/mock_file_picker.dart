e73e6f01f87e10d77d108c829a9906c06240497e
Archanciel <jp.schnyder@gmail.com>
2025-01-29 22:22:00 +0100

    added single or multiple file8s9 selection

--- integration_test/mock_file_picker.dart
+++ integration_test/mock_file_picker.dart
@@ -3,6 +3,7 @@ import 'package:file_picker/file_picker.dart';
 
 class MockFilePicker extends FilePicker {
   String _pathToSelectStr = '';
+  List<PlatformFile> _selectedFiles = [];
 
   @override
   Future<String?> getDirectoryPath({
@@ -17,5 +18,30 @@ class MockFilePicker extends FilePicker {
     required String pathToSelectStr,
   }) {
     _pathToSelectStr = pathToSelectStr;
+  }
+
+  @override
+  Future<FilePickerResult?> pickFiles({
+    FileType type = FileType.any,
+    List<String>? allowedExtensions,
+    bool allowMultiple = false,
+    bool withData = false,
+    bool allowCompression = true,
+    int compressionQuality = 100, // New parameter
+    String? dialogTitle,
+    String? initialDirectory,
+    bool lockParentWindow = false,
+    dynamic Function(FilePickerStatus)? onFileLoading, // New parameter
+    bool readSequential = false, // New parameter
+    bool withReadStream = false, // New parameter
+  }) async {
+    if (_selectedFiles.isEmpty) {
+      return null;
+    }
+    return FilePickerResult(_selectedFiles);
+  }
+
+  void setSelectedFiles(List<PlatformFile> files) {
+    _selectedFiles = files;
   }
 }
