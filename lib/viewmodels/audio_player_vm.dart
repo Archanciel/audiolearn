55b38e8663a3b7257f1b80d27e12be150fe78a6e
Archanciel <jp.schnyder@gmail.com>
2025-02-06 21:47:51 +0100

    added handling _wasAudioPlayersStopped  in modifyAudioPlayerPosition() to audioplayers_5_2_1_ALL_TESTS_PASS branch version

--- lib/viewmodels/audio_player_vm.dart
+++ lib/viewmodels/audio_player_vm.dart
@@ -1052,14 +1052,26 @@ class AudioPlayerVM extends ChangeNotifier {
   /// the audio player plugin in unit tests.
   Future<void> modifyAudioPlayerPosition({
     required Duration durationPosition,
-    bool isUndoCommandAdded = false,
+    bool isUndoCommandToAdd = false,
   }) async {
-    if (isUndoCommandAdded) {
+    if (isUndoCommandToAdd) {
       addUndoCommand(
         newDurationPosition: durationPosition,
       );
     }
 
+    if (_wasAudioPlayersStopped) {
+      // Set the source again since clicking on the pause icon
+      // stopped the audio player.
+      await _audioPlayer!
+          .setSource(DeviceFileSource(_currentAudio!.filePathName));
+
+      // Setting the value to false avoid that the audioplayers source
+      // is set again after it was re-set.
+      _wasAudioPlayersStopped = false;
+    }
+
+    _currentAudioPosition = durationPosition;
     await _audioPlayer!.seek(durationPosition);
 
     // Necessary so that the audio position is updated in the
