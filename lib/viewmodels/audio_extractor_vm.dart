11592104da4b143e06b4224de4ede6b140c7646b
Archanciel <jp.schnyder@gmail.com>
2025-02-21 13:16:47 +0100

    commented not working code

--- lib/viewmodels/audio_extractor_vm.dart
+++ lib/viewmodels/audio_extractor_vm.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart' as ffmpegKit;
-import 'package:ffmpeg_kit_flutter/return_code.dart';
-import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
+// import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart' as ffmpegKit;
+// import 'package:ffmpeg_kit_flutter/return_code.dart';
+// import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
 
 class AudioExtractorVM extends ChangeNotifier {
   /// Extracts a part of an MP3 file and saves it to another MP3 file.
@@ -19,23 +19,24 @@ class AudioExtractorVM extends ChangeNotifier {
     required double startTime,
     required double duration,
   }) async {
-    try {
-      final command =
-          '-i "$sourceMp3FilePathName" -ss $startTime -t $duration -c copy "$extractMp3FilePathName"';
+    // try {
+    //   final command =
+    //       '-i "$sourceMp3FilePathName" -ss $startTime -t $duration -c copy "$extractMp3FilePathName"';
 
-      final session = await ffmpegKit.FFmpegKit.execute(command);
-      final returnCode = await session.getReturnCode();
+    //   final session = await ffmpegKit.FFmpegKit.execute(command);
+    //   final returnCode = await session.getReturnCode();
 
-      if (returnCode != null && ReturnCode.isSuccess(returnCode)) {
-        notifyListeners();
-        return true;
-      } else {
-        debugPrint('FFmpeg execution failed with code: ${returnCode?.getValue()}');
-        return false;
-      }
-    } catch (e) {
-      debugPrint('Error extracting MP3 part: $e');
-      return false;
-    }
+    //   if (returnCode != null && ReturnCode.isSuccess(returnCode)) {
+    //     notifyListeners();
+    //     return true;
+    //   } else {
+    //     debugPrint('FFmpeg execution failed with code: ${returnCode?.getValue()}');
+    //     return false;
+    //   }
+    // } catch (e) {
+    //   debugPrint('Error extracting MP3 part: $e');
+    //   return false;
+    // }
+    return false;
   }
 }
