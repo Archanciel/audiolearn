d46eeba702e40719492426ad732e825b42f82907
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-05-10 20:29:05 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -31,7 +31,7 @@ enum AudioPopupMenuAction {
 }
 
 const String kApplicationName = "Audio Learn";
-const String kApplicationVersion = '1.4.10';
+const String kApplicationVersion = '1.4.11';
 
 // Used for Android app version
 const String kApplicationPath = "/storage/emulated/0/Documents/audiolearn";
