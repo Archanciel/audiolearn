daf3e4ca53d3ec1da247b7f5f3782cc10426150e
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-07-03 21:12:02 +0200

    works on é, à, è, î, ...

--- test/data/saved/Android_emulator_bat/get_zip_from_emulator.bat
+++ test/data/saved/Android_emulator_bat/get_zip_from_emulator.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 echo Finding zip files...
 for /f "delims=" %%i in ('adb shell "find /storage/emulated/0/Documents/test/audiolearn -name '*.zip' -type f"') do (
     echo Pulling: %%i
