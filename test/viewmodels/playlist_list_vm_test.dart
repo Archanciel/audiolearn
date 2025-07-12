1cb25bce2528a98abda6ebe648268258e88fccfa
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-07-12 09:16:08 +0200

    now, playlist list vm save mp3 to zip unit tests pass

--- test/viewmodels/playlist_list_vm_test.dart
+++ test/viewmodels/playlist_list_vm_test.dart
@@ -1211,7 +1211,7 @@ void main() {
 
         expect(
           savedZipFilePathName,
-          "$kApplicationPathWindowsTest${path.separator}audioLearn_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}.zip",
+          "$kApplicationPathWindowsTest${path.separator}audioLearn_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}_on_${yearMonthDayDateTimeFormatForFileName.format(DateTime.now().subtract(Duration(seconds: 1)))}.zip",
         );
 
         List<String> zipLst = DirUtil.listFileNamesInDir(
@@ -1306,7 +1306,7 @@ void main() {
 
         expect(
           savedZipFilePathName,
-          "$kApplicationPathWindowsTest${path.separator}audioLearn_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}.zip",
+          "$kApplicationPathWindowsTest${path.separator}audioLearn_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}_on_${yearMonthDayDateTimeFormatForFileName.format(DateTime.now())}.zip",
         );
 
         expect(
@@ -1417,7 +1417,7 @@ void main() {
 
         expect(
           savedZipFilePathName,
-          "$kApplicationPathWindowsTest${path.separator}S8 audio_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}.zip",
+          "$kApplicationPathWindowsTest${path.separator}S8 audio_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}_on_${yearMonthDayDateTimeFormatForFileName.format(DateTime.now().subtract(Duration(seconds: 1)))}.zip",
         );
 
         expect(
@@ -1527,7 +1527,7 @@ void main() {
 
         expect(
           savedZipFilePathName,
-          "$kApplicationPathWindowsTest${path.separator}http_local_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}.zip",
+          "$kApplicationPathWindowsTest${path.separator}http_local_mp3_from_${yearMonthDayDateTimeFormatForFileName.format(fromDateTime)}_on_${yearMonthDayDateTimeFormatForFileName.format(DateTime.now())}.zip",
         );
 
         expect(
