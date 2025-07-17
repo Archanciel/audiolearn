f2495ebea7e5e1fbf355db21ffde1a57f4a944a2
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-07-17 16:14:11 +0200

    added emptying target emulator dir

--- test/data/saved/Android_emulator_bat/copy_zip.bat
+++ test/data/saved/Android_emulator_bat/copy_zip.bat
@@ -4,6 +4,10 @@ REM Define parameters (modify these for each test)
 set SOURCE_PATH=C:\development\flutter\audiolearn\test\data\audio
 set DEST_PATH=/storage/emulated/0/Documents/test/audiolearn
 
+echo Cleaning target directory...
+adb shell "rm -rf %DEST_PATH%/*"
+adb shell "mkdir -p %DEST_PATH%"
+
 echo Copying files...
 for %%f in ("%SOURCE_PATH%\*.zip") do (
     echo Pushing %%~nxf
