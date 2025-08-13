ee60d1fbbcdcc1101455ee0821d1770539aba488
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-08-13 11:51:16 +0200

    Create get_files_from_emulator.bat

---
+++ test/data/saved/Android_emulator_bat/get_files_from_emulator.bat
@@ -0,0 +1,7 @@
+@echo off
+chcp 65001 >nul
+echo Finding zip files...
+for /f "delims=" %%i in ('adb shell "find /storage/emulated/0/Documents/test/audiolearn -name '*.*' -type f"') do (
+    echo Pulling: %%i
+    adb pull "%%i" "C:\development\flutter\audiolearn\test\data\audio"
+)
