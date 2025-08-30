e2be590403644c95cbf8fcb9a5c4f6ef7434c8d7
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-08-30 10:40:34 +0200

    fixed { converted to {{ bug with simplified code. works.

--- lib/viewmodels/text_to_speech_vm.dart
+++ lib/viewmodels/text_to_speech_vm.dart
@@ -129,15 +129,9 @@ class TextToSpeechVM extends ChangeNotifier {
       }
 
       // Handle the brace group
-      if (group.length == 1) {
-        for (int i = 0; i < 1; i++) {
-          result.write('{');
-        }
-      } else {
-        // Multiple consecutive braces - keep as is
-        for (int _ in group) {
-          result.write('{');
-        }
+      // Multiple consecutive braces - keep as is
+      for (int _ in group) {
+        result.write('{');
       }
 
       textIndex += group.length;
