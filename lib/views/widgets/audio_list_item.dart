38c521738027253bfbafef697be9adb98144b5a8
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-09-07 19:40:27 +0200

    cosmetic

--- lib/views/widgets/audio_list_item.dart
+++ lib/views/widgets/audio_list_item.dart
@@ -765,21 +765,22 @@ class AudioListItem extends StatelessWidget with ScreenMixin {
       case SortingOption.videoUploadDate:
         // This video upload date value is only used if the audio type is
         // AudioType.imported or AudioType.converted.
-        DateTime videoUploadDate = DateTime(0, 1, 1,);
+        DateTime videoUploadDate = DateTime(0, 1, 1);
         final String lastSubtitlePart;
 
         if (audio.audioType == AudioType.downloaded) {
           videoUploadDate = audio.videoUploadDate;
         }
 
-        String formatedDate = dateFormatVMlistenTrue.formatDate(videoUploadDate);
+        String formatedDate =
+            dateFormatVMlistenTrue.formatDate(videoUploadDate);
 
         if (formatedDate.contains('0000')) {
           formatedDate = formatedDate.replaceAll('01', '00');
         }
 
         lastSubtitlePart =
-            '${AppLocalizations.of(context)!.videoUploadDate}: ${formatedDate}';
+            '${AppLocalizations.of(context)!.videoUploadDate}: $formatedDate';
 
         return '${audioDuration.HHmmss(addRemainingOneDigitTenthOfSecond: true)} $lastSubtitlePart';
       case SortingOption.audioDownloadDuration:
