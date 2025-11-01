d1d6894aca5992a2a2d1789e09d6ea3d9817442d
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-11-01 07:17:04 +0100

    moved fullyListenedBufferSeconds  to constant on main version

--- lib/viewmodels/audio_player_vm.dart
+++ lib/viewmodels/audio_player_vm.dart
@@ -1023,7 +1023,7 @@ class AudioPlayerVM extends ChangeNotifier {
     if (newAudioPosition > Duration.zero &&
         newAudioPosition <
             (_currentAudio!.audioDuration -
-                Duration(seconds: Audio.fullyListenedBufferSeconds))) {
+                Duration(seconds: fullyListenedBufferSeconds))) {
       _currentAudio!.isPlayingOrPausedWithPositionBetweenAudioStartAndEnd =
           true;
     } else {
