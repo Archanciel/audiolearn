6e03b0b5d8c48f4f104d6b0c839228d949b197f9
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-01-17 21:48:48 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ Future<void> main() async {
   WidgetsFlutterBinding
       .ensureInitialized(); // Ensure Flutter bindings are initialized.
 
-  bool isTest = false; // Must be set to false instead of true before
+  bool isTest = true; // Must be set to false instead of true before
   //                     generating the Android as well as the Windows
   //                     version of the app so that the app accesses the
   //                     correct application directory and not the test
