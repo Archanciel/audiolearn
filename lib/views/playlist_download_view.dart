9568def99f82d992ba058a8f549c8929c39fd597
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-02-02 13:32:32 +0100

    removed unnecessary info

--- lib/views/playlist_download_view.dart
+++ lib/views/playlist_download_view.dart
@@ -661,14 +661,6 @@ class _PlaylistDownloadViewState extends State<PlaylistDownloadView>
                 ),
                 const SizedBox(height: 10.0),
                 LinearProgressIndicator(), // Indeterminate progress bar
-                const SizedBox(height: 10.0),
-                Text(
-                  key: const Key('saving_please_wait'),
-                  AppLocalizations.of(context)!.savingApproximativeTime(
-                      playlistListVMlistenTrue.savingAudioMp3FileToZipDuration
-                          .HHmmss(),
-                      playlistListVMlistenTrue.numberOfCreatedZipFiles),
-                ),
               ],
             ),
           );
