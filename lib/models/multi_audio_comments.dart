73c81ae58a8e70b7cc2d94e74a57b9e6afc1b206
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-02-09 15:30:12 +0100

    deleted unusefull code. save and load multi audio comments works.

--- lib/models/multi_audio_comments.dart
+++ lib/models/multi_audio_comments.dart
@@ -21,38 +21,14 @@ class MultiAudioComments {
   factory MultiAudioComments.fromJson(Map<String, dynamic> json) {
     final Map<String, List<Comment>> map = {};
 
-    try {
-      final audioCommentsMap =
-          json['audioCommentsMap'] as Map<String, dynamic>?;
+    final audioCommentsMap = json['audioCommentsMap'] as Map<String, dynamic>;
+    audioCommentsMap.forEach((audioFileName, commentsList) {
+      final List<Comment> comments = (commentsList as List)
+          .map((commentJson) => Comment.fromJson(commentJson))
+          .toList();
+      map[audioFileName] = comments;
+    });
 
-      if (audioCommentsMap == null) {
-        throw Exception('Missing audioCommentsMap in JSON');
-      }
-
-      audioCommentsMap.forEach((audioFileName, commentsList) {
-        if (commentsList is! List) {
-          throw Exception('Invalid comments list for $audioFileName');
-        }
-
-        final List<Comment> comments = [];
-        for (var commentJson in commentsList) {
-          try {
-            comments.add(Comment.fromJson(commentJson as Map<String, dynamic>));
-          } catch (e) {
-            print('Error parsing comment in $audioFileName: $e');
-            // Skip this comment but continue with others
-          }
-        }
-
-        if (comments.isNotEmpty) {
-          map[audioFileName] = comments;
-        }
-      });
-
-      return MultiAudioComments(audioCommentsMap: map);
-    } catch (e) {
-      print('Error in MultiAudioComments.fromJson: $e');
-      rethrow;
-    }
+    return MultiAudioComments(audioCommentsMap: map);
   }
 }
