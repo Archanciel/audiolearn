6c4e1e45902db0579e1421fd13eb9af0bfaa668f
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2026-03-28 08:13:26 +0100

    docu

--- lib/views/widgets/comment_list_add_dialog.dart
+++ lib/views/widgets/comment_list_add_dialog.dart
@@ -1301,7 +1301,7 @@ class _CommentListAddDialogContentState
 
     await audioPlayerVMlistenFalse.modifyAudioPlayerPosition(
       durationPosition: Duration(
-          milliseconds: comment.commentStartPositionInTenthOfSeconds * 100), // 420 * 100 = 42,000 ms = 42 seconds
+          milliseconds: comment.commentStartPositionInTenthOfSeconds * 100),
       isUndoCommandToAdd: true,
     );
 
@@ -1316,7 +1316,7 @@ class _CommentListAddDialogContentState
     await audioPlayerVMlistenFalse.playCurrentAudio(
       rewindAudioPositionBasedOnPauseDuration: false,
       commentEndPositionInTenthOfSeconds:
-          comment.commentEndPositionInTenthOfSeconds, // 460
+          comment.commentEndPositionInTenthOfSeconds,
     );
   }
 
